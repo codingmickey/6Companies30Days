55ec865f02bf3d950f5acb0b266396e7f9012fab
Kartik Jolapara <42518907+codingmickey@users.noreply.github.com>
2023-01-07 00:59:59 +0530

    Update RotateFunction.cpp

--- 1.Microsoft/RotateFunction.cpp
+++ 1.Microsoft/RotateFunction.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int curMax = INT_MIN;
+        int pro = 0, sum = 0;
+        if(nums.size() == 1) return 0;
+        for(int i = 0; i < nums.size(); i++) {
+            pro += nums[i] * i;
+            sum += nums[i];
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            curMax = pro > curMax ? pro : curMax;
+            pro += (-(nums.size() -1) * nums[nums.size() - i - 1]);
+            pro += (sum - nums[nums.size() - i - 1]);
+        }
+        return curMax;
+    }
+};
